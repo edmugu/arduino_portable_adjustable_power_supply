992bfb7eaf24139308b36597ffa2f0d38b85ac23
Eduardo Munoz-Gutierrez <edmugu@protonmail.com>
2021-02-16 20:00:54 -0800

    Update read_voltage.py

--- python/read_voltage.py
+++ python/read_voltage.py
@@ -22,10 +22,11 @@ import time
 
 version = "0.1"
 
-scale = 13.38
+
 arduino_vcc = 5.0
-port_list = ["3", "2", "1"]
-name_list = {"3": "Vout", "2": "Vstep_up", "1": "Vin"}
+pin_list = ["3", "2", "1"]
+pin_name = {"3": "Vout", "2": "Vstep_up", "1": "Vin"}
+pin_scale = {"Vout": 13.38, "Vstep_up": 13.38, "Vin": 3}
 
 
 print("\ntools_read_voltage.py version %s" % version)
@@ -34,14 +35,14 @@ print("FOR HELP TYPE tools_read_voltage.py --help\n\n")
 from pyfirmata import Arduino, util
 
 
-def read_voltage(arduino_port="COM3"):
+def read_voltage(port="COM3"):
     """
     It reads the voltage expecified by 'arduion_port'. 
     EXAMPLE: 
         python tools_read_voltage.py --arduion_port COM3
     """
-    print("trying to connect to arduino on %s" % arduino_port)
-    board = Arduino(arduino_port)
+    print("trying to connect to arduino on %s" % port)
+    board = Arduino(port)
     board.digital[13].write(1)
     print("connected succesfully!")
     print("the board firmware is version %s.%s" % board.firmware_version)
@@ -51,27 +52,28 @@ def read_voltage(arduino_port="COM3"):
     it.start()
 
     a = {}
-    for p in port_list:
+    for p in pin_list:
         board.analog[int(p)].enable_reporting()
     time.sleep(0.1)
 
     print("reading the raw voltages\n")
-    for p in port_list:
+    for p in pin_list:
         a[p] = board.analog[int(p)].read()
         for read_tries in range(99):
             if a[p] is not None:
+                vcc_percent = a[p]
                 a[p] = a[p] * arduino_vcc
-                print("Read %3.3f Volts on pin a%s" % (a[p], p))
+                print("Read %3.2f percent of VCC on pin a%s (= %3.2f Volts)" % (vcc_percent, p, a[p]))
                 break
 
         if a[p] is None:
             print("Read NONE on pin a%s" % p)
 
     print("\nCalculating the voltage on the board")
-    for p in port_list:
+    for p in pin_list:
         if a[p] is not None:
-            tmp = a[p] * scale
-            print("%s is %3.3f" % (name_list[p], tmp))
+            tmp = a[p] * pin_scale[pin_name[p]]
+            print("%9s is %#05.2f Volts" % (pin_name[p], tmp))
 
 
 if __name__ == "__main__":
