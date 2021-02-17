dad2240287771ac46f5a8149a48bacac0e01e5cb
Eduardo Munoz-Gutierrez <edmugu@protonmail.com>
2021-02-16 19:36:40 -0800

    Update read_voltage.py
    Ran BLACK on file

--- python/read_voltage.py
+++ python/read_voltage.py
@@ -17,7 +17,7 @@ REQUIREMENTS:
 @email: edmugu@protonmail.com
 """
 import pyfirmata
-import fire 
+import fire
 import time
 
 version = "0.1"
@@ -34,7 +34,6 @@ print("FOR HELP TYPE tools_read_voltage.py --help\n\n")
 from pyfirmata import Arduino, util
 
 
-
 def read_voltage(arduino_port="COM3"):
     """
     It reads the voltage expecified by 'arduion_port'. 
@@ -46,37 +45,34 @@ def read_voltage(arduino_port="COM3"):
     board.digital[13].write(1)
     print("connected succesfully!")
     print("the board firmware is version %s.%s" % board.firmware_version)
-    
-    
+
     print("Enabling the IO \n")
     it = util.Iterator(board)
     it.start()
-    
+
     a = {}
     for p in port_list:
         board.analog[int(p)].enable_reporting()
-    time.sleep(.1)
-    
-    
+    time.sleep(0.1)
+
     print("reading the raw voltages\n")
     for p in port_list:
         a[p] = board.analog[int(p)].read()
         for read_tries in range(99):
             if a[p] is not None:
                 a[p] = a[p] * arduino_vcc
-                print("Read %3.3f Volts on pin a%s" %(a[p], p))
+                print("Read %3.3f Volts on pin a%s" % (a[p], p))
                 break
-            
+
         if a[p] is None:
             print("Read NONE on pin a%s" % p)
-            
-        
-    print("\nCalculating the voltage on the board")   
+
+    print("\nCalculating the voltage on the board")
     for p in port_list:
         if a[p] is not None:
             tmp = a[p] * scale
             print("%s is %3.3f" % (name_list[p], tmp))
-    
-        
-if __name__ == '__main__':
-  fire.Fire(read_voltage)
+
+
+if __name__ == "__main__":
+    fire.Fire(read_voltage)
